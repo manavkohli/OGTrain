fdad80c39caf09e7ba0ce1ed1b0b741d77ad6f8f
manavkohli <manavk123@gmail.com>
2015-09-30 00:15:15 -0700

    added API example response and improved API call

--- App/OGTrain/OGTrain/YelpAPISample/YPAPISample.m
+++ App/OGTrain/OGTrain/YelpAPISample/YPAPISample.m
@@ -3,6 +3,7 @@
 //  YelpAPI
 
 #import "YPAPISample.h"
+#import <Foundation/Foundation.h>
 
 /**
  Default paths and search terms used in this example
@@ -19,7 +20,15 @@ static NSString * const kSearchLimit       = @"3";
 /**
  Returns an array of the top result names for a query
  Completion handler is used to identify HTTP response codes
+ 
+ Arguments:
+ term               -- Search term (e.g. "food", "restaurants"
+ location           -- Location to search restaurants by
+ radius             -- Radius to search for goods/services within
+ results            -- Number of results to return
+ category           -- Category of good/service
  */
+
 
 - (void) listResults:(NSString *)term location:(NSString *)location radius:(NSString *)radius results:(NSString *)results category:(NSString*)category completionHandler:(void (^)(NSDictionary *businessArray, NSError *error))completionHandler {
 
@@ -47,12 +56,35 @@ static NSString * const kSearchLimit       = @"3";
             
             NSDictionary *searchResponseJSON = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
             NSArray *businessArray = searchResponseJSON[@"businesses"];
-//            for(int i = 0; i < [businessArray length]; i++){
-//                NSLog(@"%", businessArray[i][@"name"]);
-//            }
-            //Access the name
-            NSString *name = businessArray[0][@"name"];
-            NSLog(@"%@", name);
+            
+            //Contains a list of all the restaurants
+            NSMutableArray *restArray = [[NSMutableArray alloc] initWithCapacity: results];
+            
+            
+            for(int i = 0; i < [businessArray count]; i=i+1){
+                NSMutableArray *restResults = [[NSMutableArray alloc] initWithCapacity: 4];
+                [restResults insertObject:businessArray[i][@"name"] atIndex:0];
+                [restResults insertObject:businessArray[i][@"mobile_url"] atIndex:1];
+                [restResults insertObject:businessArray[i][@"rating"] atIndex:2];
+                [restResults insertObject:businessArray[i][@"location"][@"address"] atIndex:3];
+                [restArray  insertObject:restResults atIndex:i];
+                
+                
+                
+                NSString *name = businessArray[i][@"name"];
+//                NSString *rating = businessArray[i][@"rating"];
+//                NSString *mobileURL = businessArray[i][@"mobile_url"];
+//                NSString *address = businessArray[i][@"location"][@"address"];
+                
+                //NOTE: some addresses have more info (i.e. lats/longs) than others
+                
+                NSLog(@"%@", name);
+                
+//                NSLog(@"%s", businessArray[i][@"name"]);
+            }
+//            //Access the name
+//            NSString *name = businessArray[1][@"name"];
+//            NSLog(@"%@", name);
             
         }
         // response code != 200; there was an error
