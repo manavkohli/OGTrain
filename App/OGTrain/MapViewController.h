16ce083b32ee3d67f6461cba9bbcc87ed941d3dd
Dhruv Manchala <dmanchala16@students.claremontmckenna.edu>
2015-10-14 21:24:19 -0700

    Added map and my location

---
+++ App/OGTrain/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  OGTrain
+//
+//  Created by Dhruv Manchala on 10/11/15.
+//  Copyright (c) 2015 Manav Kohli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
